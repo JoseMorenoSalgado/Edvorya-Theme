b20f6e3d726b156a2f75db38dd4994d18b660076
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 12:42:37 -0600

    feat: add embedded and popup layout

---
+++ layout/embedded.php
@@ -0,0 +1,20 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+/**
+ * Embedded and popup layout.
+ *
+ * @package    theme_edvorya
+ * @copyright  2026 Elearning Cloud
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$templatecontext = [
+    'output' => $OUTPUT,
+    'bodyattributes' => $OUTPUT->body_attributes(),
+    'maincontent' => $OUTPUT->main_content(),
+];
+
+echo $OUTPUT->render_from_template('theme_edvorya/layout/embedded', $templatecontext);
