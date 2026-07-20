d369df2017e02bb6aa9feec5cd8490dd3ed03a47
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 12:41:20 -0600

    feat: add Moodle 5.2 theme version metadata

---
+++ version.php
@@ -0,0 +1,23 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Version details for theme_edvorya.
+ *
+ * @package    theme_edvorya
+ * @copyright  2026 Elearning Cloud
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'theme_edvorya';
+$plugin->version = 2026072000;
+$plugin->requires = 2026042000;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1.0-alpha';
