4b73dcc70fc94b089a05ae9f844f0faa290ef605
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 12:41:27 -0600

    feat: add English language strings

---
+++ lang/en/theme_edvorya.php
@@ -0,0 +1,17 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+/**
+ * English language strings for theme_edvorya.
+ *
+ * @package    theme_edvorya
+ * @copyright  2026 Elearning Cloud
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Edvorya';
+$string['choosereadme'] = 'Edvorya is a standalone Moodle theme built around the Edvorya Design System.';
+$string['configtitle'] = 'Edvorya';
+$string['privacy:metadata'] = 'The Edvorya theme does not store personal data.';
