7d4e4d48a9fa5cc1752402138fde9f71302b415d
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 12:41:14 -0600

    feat: add standalone Moodle theme configuration

---
+++ config.php
@@ -0,0 +1,120 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Edvorya theme configuration.
+ *
+ * @package    theme_edvorya
+ * @copyright  2026 Elearning Cloud
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$THEME->name = 'edvorya';
+$THEME->parents = [];
+$THEME->sheets = ['edvorya'];
+$THEME->editor_sheets = [];
+$THEME->doctype = 'html5';
+$THEME->hidefromselector = false;
+
+$THEME->layouts = [
+    'base' => [
+        'file' => 'drawers.php',
+        'regions' => [],
+    ],
+    'standard' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'course' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'coursecategory' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'incourse' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'frontpage' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'admin' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'mycourses' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'mydashboard' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'mypublic' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'login' => [
+        'file' => 'login.php',
+        'regions' => [],
+        'options' => ['langmenu' => true, 'nonavbar' => true],
+    ],
+    'popup' => [
+        'file' => 'embedded.php',
+        'regions' => [],
+        'options' => ['nofooter' => true, 'nonavbar' => true],
+    ],
+    'embedded' => [
+        'file' => 'embedded.php',
+        'regions' => [],
+        'options' => ['nofooter' => true, 'nonavbar' => true],
+    ],
+    'frametop' => [
+        'file' => 'embedded.php',
+        'regions' => [],
+        'options' => ['nofooter' => true, 'nonavbar' => true],
+    ],
+    'print' => [
+        'file' => 'embedded.php',
+        'regions' => [],
+        'options' => ['nofooter' => true, 'nonavbar' => false],
+    ],
+    'redirect' => [
+        'file' => 'embedded.php',
+        'regions' => [],
+        'options' => ['nofooter' => true, 'nonavbar' => true],
+    ],
+    'report' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'secure' => [
+        'file' => 'drawers.php',
+        'regions' => ['side-pre'],
+        'defaultregion' => 'side-pre',
+    ],
+    'maintenance' => [
+        'file' => 'maintenance.php',
+        'regions' => [],
+    ],
+];
