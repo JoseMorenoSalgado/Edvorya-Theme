e74ce9dba5f3c2fcbb7269a2ec5acac6c4e08807
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 13:25:45 -0600

    chore: bump statically validated foundation alpha

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_edvorya';
-$plugin->version = 2026072000;
+$plugin->version = 2026072001;
 $plugin->requires = 2026042000;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.1.0-alpha';
+$plugin->release = '0.1.0-alpha.1';
