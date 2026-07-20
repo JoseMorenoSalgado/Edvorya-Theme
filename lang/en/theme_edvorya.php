0a2407dc1a12dd87c6817a678147c58ea6866029
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 12:44:02 -0600

    fix: add missing theme settings language strings

--- lang/en/theme_edvorya.php
+++ lang/en/theme_edvorya.php
@@ -14,4 +14,6 @@ defined('MOODLE_INTERNAL') || die();
 $string['pluginname'] = 'Edvorya';
 $string['choosereadme'] = 'Edvorya is a standalone Moodle theme built around the Edvorya Design System.';
 $string['configtitle'] = 'Edvorya';
+$string['generalsettings'] = 'General settings';
+$string['generalsettings_desc'] = 'Core visual and behaviour settings for the Edvorya theme will be added here as the design system evolves.';
 $string['privacy:metadata'] = 'The Edvorya theme does not store personal data.';
