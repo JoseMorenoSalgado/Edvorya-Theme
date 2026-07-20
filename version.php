225a506cbd62594035eb9777b01d6616acf189cc
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 17:44:42 -0600

    chore: bump responsive accessibility fix alpha

--- version.php
+++ version.php
@@ -17,7 +17,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_edvorya';
-$plugin->version = 2026072003;
+$plugin->version = 2026072004;
 $plugin->requires = 2026042000;
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.1.0-alpha.3';
+$plugin->release = '0.1.0-alpha.4';
