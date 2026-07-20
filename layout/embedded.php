5b997565c941632b22c3a15e4b1dfd1b07866b43
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 13:00:29 -0600

    refactor: inject branding into embedded layouts

--- layout/embedded.php
+++ layout/embedded.php
@@ -1,8 +1,13 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 
 /**
- * Embedded and popup layout.
+ * Embedded, popup, print, frame and redirect layout.
  *
  * @package    theme_edvorya
  * @copyright  2026 Elearning Cloud
@@ -11,9 +16,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$branding = (new \theme_edvorya\output\branding())->export_for_template($OUTPUT);
+
 $templatecontext = [
     'output' => $OUTPUT,
-    'bodyattributes' => $OUTPUT->body_attributes(),
+    'bodyattributes' => $OUTPUT->body_attributes(['edv-context-embedded']),
+    'branding' => $branding,
     'maincontent' => $OUTPUT->main_content(),
 ];
 
