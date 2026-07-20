e7f6437e4dc9f3cf53b82a07898e095cf481c983
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 12:54:25 -0600

    feat: add dedicated secure Moodle layout

---
+++ layout/secure.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+/**
+ * Secure and safe-browser layout.
+ *
+ * @package    theme_edvorya
+ * @copyright  2026 Elearning Cloud
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$blockshtml = $OUTPUT->blocks('side-pre');
+$hasblocks = strpos($blockshtml, 'data-block=') !== false;
+$corerenderer = $PAGE->get_renderer('core');
+$headercontent = $PAGE->activityheader->export_for_template($corerenderer);
+
+$templatecontext = [
+    'output' => $OUTPUT,
+    'bodyattributes' => $OUTPUT->body_attributes(['edv-context-secure']),
+    'fullheader' => $OUTPUT->full_header(),
+    'maincontent' => $OUTPUT->main_content(),
+    'sidepreblocks' => $blockshtml,
+    'hasblocks' => $hasblocks,
+    'headercontent' => $headercontent,
+];
+
+echo $OUTPUT->render_from_template('theme_edvorya/layout/secure', $templatecontext);
