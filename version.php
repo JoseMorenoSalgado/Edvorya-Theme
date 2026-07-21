e429efc92eaa16681982f968b78b3147e5b35e91
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-21 06:53:13 -0600

    chore: release alpha.10 intent-first dashboards

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 /**
  * Version details for theme_edvorya.
  *
- * Alpha.9 aligns the mobile drawer, topbar actions and navigation iconography with Edvorya LMS.
+ * Alpha.10 introduces intent-first student and teacher dashboard experiences aligned with Edvorya LMS.
  *
  * @package    theme_edvorya
  * @copyright  2026 Elearning Cloud
@@ -19,8 +19,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_edvorya';
-$plugin->version = 2026072009;
+$plugin->version = 2026072010;
 $plugin->requires = 2025041400; // Moodle 5.0.0.
 $plugin->supported = [500, 502]; // Moodle 5.0 through Moodle 5.2, inclusive.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.1.0-alpha.9';
+$plugin->release = '0.1.0-alpha.10';
