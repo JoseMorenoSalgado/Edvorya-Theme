c2aed057c7ec58767fd68046d97d72ea043812b9
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-21 06:51:38 -0600

    feat: load dedicated dashboard experience styles

--- config.php
+++ config.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $THEME->name = 'edvorya';
 $THEME->parents = [];
-$THEME->sheets = ['edvorya', 'edvorya-compat'];
+$THEME->sheets = ['edvorya', 'edvorya-compat', 'edvorya-dashboard'];
 $THEME->csspostprocess = 'theme_edvorya_css_post_process';
 $THEME->editor_sheets = [];
 $THEME->doctype = 'html5';
