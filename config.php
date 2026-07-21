67607a0fb9790e93730691ac1d34a65054322225
Elearning Cloud <94720805+JoseMorenoSalgado@users.noreply.github.com>
2026-07-20 22:46:32 -0600

    fix: load standalone compatibility stylesheet

--- config.php
+++ config.php
@@ -18,7 +18,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $THEME->name = 'edvorya';
 $THEME->parents = [];
-$THEME->sheets = ['edvorya'];
+$THEME->sheets = ['edvorya', 'edvorya-compat'];
 $THEME->csspostprocess = 'theme_edvorya_css_post_process';
 $THEME->editor_sheets = [];
 $THEME->doctype = 'html5';
